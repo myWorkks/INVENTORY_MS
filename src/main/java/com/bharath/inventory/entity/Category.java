7512f2a1ebefb198515f1e543c70eed97667fa9c
Bharath Simha Reddy Narsannagari <bharathsimharn@tblocks.com>
2024-07-06 23:26:07 +0530

    category entity updated

--- src/main/java/com/bharath/inventory/entity/Category.java
+++ src/main/java/com/bharath/inventory/entity/Category.java
@@ -1,11 +1,15 @@
 package com.bharath.inventory.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,7 +23,7 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long categoryId;
 private String categoryName;
-@ManyToOne
+@OneToMany(cascade = CascadeType.ALL)
 @JoinColumn(name="subcategory_id")
-private Category subCategory;
+private List<Category> subCategory;
 }
